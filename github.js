a577e13f986afe60e935e87d2f1c613d34a28b4b
Saimon Sangma <saimonsinghsangma@gmail.com>
2026-09-15 16:42:30 +0530

    initial

---
+++ github.js
@@ -0,0 +1,8 @@
+function introduce(name, age, course) {
+    console.log("Hello!");
+    console.log("My name is " + name);
+    console.log("I am " + age + " years old");
+    console.log("I am studying " + course);
+}
+
+introduce("Saimon", 18, "BCA");
