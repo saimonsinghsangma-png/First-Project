f53c7ffd752d93a5cd7b1eee7243baab6d12e2f7
Saimon Sangma <saimonsinghsangma@gmail.com>
2026-09-22 16:54:33 +0530

    Add files via upload

---
+++ task1.js
@@ -0,0 +1,31 @@
+// Student Information
+
+let studentName = "saimon";
+let age = 18;
+var course = "BCA";
+const collegeName = "SITM";
+let marks = 75;
+var birthYear = 2008;
+
+// Bonus
+let city = "Tura";
+let semester = 1;
+const favouriteSubject = "JavaScript";
+
+// Change two values
+age = 19;
+marks = 80;
+
+// Change a value that should not be changed
+// collegeName = "ABC College"; // Error: Assignment to constant variable
+
+// Display final information
+console.log("Student Name:", studentName);
+console.log("Age:", age);
+console.log("Course:", course);
+console.log("College Name:", collegeName);
+console.log("Marks:", marks);
+console.log("Birth Year:", birthYear);
+console.log("City:", city);
+console.log("Semester:", semester);
+console.log("Favourite Subject:", favouriteSubject);
